2d57d786c524046517af01a1de9a7051691ffe35
AryanPROFFESOR <145572344+AryanPROFFESOR@users.noreply.github.com>
2025-07-09 12:24:41 +0530

    Update hero.js

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -59,29 +59,22 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const one = <h1>HMG Lab</h1>;
+  const two = <h2 className="big-heading">Dr. Anuranjan Anand.</h2>;
+  const three = <h3 className="big-heading">Human Molecular Genetics Lab.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        Welcome to the Human Molecular Genetics Lab. Our Lab is dedicated to conducting cutting edge research in the field of human molecular genetics. We are committed to unraveling the complexities of the human genome and understanding the genetic basis of various diseases and traits.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
+      href="#about"
       rel="noreferrer">
-      Check out my course!
+      See my work below!
     </a>
   );
 
